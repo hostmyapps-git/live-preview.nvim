e6b423c1fff48cdc43c77e7f5e01bd90f40b9a61
andy <andy@mbprom4>
2026-07-07 21:18:31 +0200

    cleanup

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,17 +1,9 @@
 import { bootstrapLibraries } from "./bootstrap.js";
 import { connectWebSocket } from "./websocket.js";
-import { renderContent } from "./render/markdown.js";
-import { renderMermaidBlocks, loadMermaidIconPacks } from "./render/mermaid.js";
-import { ensurePlantUmlControls, renderPlantUML, updatePlantUmlImages } from "./render/plantuml.js";
-import { renderGraphviz } from "./render/graphviz.js";
-import { renderChartJs } from "./render/chart.js";
-import { renderD2 } from "./render/d2.js";
-import { renderMath } from "./render/katex.js";
-import { highlightAll } from "./render/highlight.js";
-import { renderSvgImgBlocks } from "./render/svgimg.js";
-import { renderTikzBlocks } from "./render/tikz.js";
+import { renderContent } from "./render/base.js";
+import { postProcessDocument } from "./render/pipeline.js";
+import { loadMermaidIconPacks } from "./render/mermaid.js";
 import { loadSVGIconPacks } from "./svg/icons.js";
-import { appendInlinedStyles, appendInlinedScripts, smoothScrollTo,  attachSvgDownloadToolbars } from "./utils.js";
 
 const theMother = document.querySelector("#theMother");
 let mermaidIconsRegistered = false;
@@ -75,9 +67,7 @@ export async function handleMessage(data) {
 		const cfg = data.config || {};
 		applyLuaConfig(cfg);
 		bootstrapLibraries(cfg);
-		let html = renderContent(data);
-		theMother.innerHTML = html;
-
+		theMother.innerHTML = renderContent(data);
 		if (!(mermaidIconsRegistered && svgIconsRegistered)) {
 			await loadIconPacks(cfg);
 		}
@@ -88,22 +78,7 @@ export async function handleMessage(data) {
 				svgEl.setAttribute("height", "auto");
 			}
 		}
-
-		// call Renderer
-		await renderMermaidBlocks(data.format);
-		await renderGraphviz();
-		await renderChartJs();
-		renderPlantUML();
-		renderTikzBlocks(data.format);
-		renderSvgImgBlocks(data.format);
-		await renderD2(data.format);
-		renderMath(theMother);
-		highlightAll();
-		// insert download buttons
-		attachSvgDownloadToolbars();
-		appendInlinedStyles(cfg);
-		appendInlinedScripts(cfg);
-		smoothScrollTo(data);
+		await postProcessDocument(data, cfg, theMother);
 		setupThemeDropdown(cfg.general?.defaultAppearance || "light");
 	} catch (err) {
 		theMother.innerHTML = `<pre>Parse Error: ${err.message}</pre>`;
---
+++ static/scripts/render/pipeline.js
@@ -0,0 +1,37 @@
+import { renderMermaidBlocks } from "./mermaid.js";
+import { renderGraphviz } from "./graphviz.js";
+import { renderChartJs } from "./chart.js";
+import { renderPlantUML } from "./plantuml.js";
+import { renderTikzBlocks } from "./tikz.js";
+import { renderSvgImgBlocks } from "./svgimg.js";
+import { renderD2 } from "./d2.js";
+import { renderMath } from "./katex.js";
+import { highlightAll } from "./highlight.js";
+import { appendInlinedStyles, appendInlinedScripts, smoothScrollTo, attachSvgDownloadToolbars } from "../utils.js";
+
+export async function postProcessDocument(data, cfg, root) {
+	const format = data?.format || "markdown";
+	const steps = [
+		() => renderMermaidBlocks(format),
+		() => renderGraphviz(),
+		() => renderChartJs(),
+		() => renderPlantUML(),
+		() => renderTikzBlocks(format),
+		() => renderSvgImgBlocks(format),
+		() => renderD2(format),
+		() => renderMath(root),
+		() => highlightAll(),
+		() => attachSvgDownloadToolbars(),
+		() => appendInlineStyles(cfg),
+		() => appendInlineScripts(cfg),
+		() => smoothScrollTo(data),
+	];
+	for (const step of steps) {
+		try{
+			await step();
+		}
+		catch (err){
+			console.error(err);
+		}
+	}
+}
